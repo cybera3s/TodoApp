9d5301e3bf0a8a4db563afd27855b26d072c2e9e
ario <cybera.3s@gmail.com>
2022-10-15 15:38:44 +0330

    mount static files

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI, Depends
 import models
 from database import engine
 from routers import auth, todos, users, address
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth.router)
 app.include_router(todos.router)
