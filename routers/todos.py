da0db671930cda7352875dd22abdc26c49501353
ario <cybera.3s@gmail.com>
2022-10-20 13:44:52 +0330

    refactor context for get pages

--- routers/todos.py
+++ routers/todos.py
@@ -43,7 +43,13 @@ async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
     todos = db.query(models.Todos) \
         .filter(models.Todos.owner_id == user.get('id')).all()
 
-    return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
+    context = {
+        "request": request,
+        "todos": todos,
+        "user": user
+    }
+
+    return templates.TemplateResponse("home.html", context)
 
 
 @router.get('/add-todo', response_class=HTMLResponse)
@@ -53,7 +59,12 @@ async def add_new_todo(request: Request):
     if user is None:
         return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
 
-    return templates.TemplateResponse('add-todo.html', {'request': request})
+    context = {
+        'request': request,
+        "user": user
+    }
+
+    return templates.TemplateResponse('add-todo.html', context)
 
 
 @router.post('/add-todo', response_class=HTMLResponse)
@@ -87,7 +98,13 @@ async def edit_todo(request: Request, todo_id: int, db: Session = Depends(get_db
     todo = db.query(models.Todos) \
         .filter(models.Todos.id == todo_id).first()
 
-    return templates.TemplateResponse('edit-todo.html', {'request': request, "todo": todo})
+    context = {
+        'request': request,
+        "todo": todo,
+        "user": user
+    }
+
+    return templates.TemplateResponse('edit-todo.html', context)
 
 
 @router.post("/edit-todo/{todo_id}", response_class=HTMLResponse)
